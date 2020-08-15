88b011763b6233dde39458c8267724971e25e8d5
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-08-15 00:39:18 -0300

    - Adicionado middleware auth, rota /
    - Criado um usário direto na migration users, somente para testes.
    
    email: admin@admin.com.br
    senha: 12345678

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,14 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+                'name' => 'admin',
+                'email' => 'admin@admin.com.br',
+                'email_verified_at' => now(),
+                'password' => bcrypt('12345678')
+            ]
+        );
     }
 
     /**
