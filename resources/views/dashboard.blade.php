61dd2a8991aa29ebde936c1714a73ffe2e54bdcd
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-08-15 14:42:56 -0300

    - Inclusão nova versão modelagem DB
    - Ajuste layout
    
    - Ajuste Tradução pt_BR

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.app')
+
+@section('content_header_title', 'Dashboard')
+
+@section('content_header_breadcrumb')
+
+    {{--<li class="breadcrumb-item"><a href="#">Home</a></li>--}}
+    <li class="breadcrumb-item active">Dashboard</li>
+
+@endsection
+
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-3">
+            <!-- small card -->
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h5><b>Residências</b></h5>
+
+                    <h3>236</h3>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-chart-pie"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    Listar <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <!-- small card -->
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h5><b>Usuários</b></h5>
+
+                    <h3>587</h3>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-users"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    Listar <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <br>
+
+    <h4 class="mb-2">Ocorrências</h4>
+    <div class="row">
+        <div class="col-md-3">
+            <!-- small card -->
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>65</h3>
+
+                    <h5><b>Abertas</b></h5>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-chart-pie"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    Listar <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <!-- small card -->
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>38</h3>
+
+                    <h5><b>Em andamento</b></h5>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-chart-pie"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    Listar <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <br>
+
+    <h4 class="mb-2">Correspondências</h4>
+    <div class="row">
+        <div class="col-md-3">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>61</h3>
+
+                    <h5><b>Pendentes</b></h5>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-envelope"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    Listar <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <li class="nav-item dropdown">
                 <a class="nav-link" data-toggle="dropdown" href="#">
                     <i class="far fa-bell"></i>
-                    <span class="badge badge-warning navbar-badge">15</span>
+                    <span class="badge badge-primary navbar-badge">15</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                     <span class="dropdown-header">15 Notifications</span>
@@ -181,15 +181,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">Dashboard</h1>
+                        <h1 class="m-0 text-dark"><b>@yield('content_header_title')</b></h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard</li>
+                            @yield('content_header_breadcrumb')
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
+                <hr>
             </div><!-- /.container-fluid -->
         </div>
         <!-- /.content-header -->
