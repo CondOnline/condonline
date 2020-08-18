d4eaa31a1c3fa26cf0f609b657d64c359053b3a1
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-08-18 01:33:39 -0300

    - ajuste email

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -6,6 +6,8 @@
 
         <form action="{{ route('password.update') }}" method="post">
             @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
+
             <div class="input-group mb-3">
                 <input id="email" type="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
                 <div class="input-group-append">
