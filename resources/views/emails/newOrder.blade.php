960525579d9ffbc89f6c804b98bae51c52d790c5
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-08-27 00:23:02 -0300

    - Criar notificação nova encomenda

--- resources/views/emails/newOrder.blade.php
+++ resources/views/emails/newOrder.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
-<p> Olá , </p>
-<p> sua encomenda com rastreio: , acaba de ser recebida. </p>
+<p> Olá {{$order->user->name}}, </p>
+<p> sua encomenda com rastreio: {{ $order->tracking }}, acaba de ser recebida. </p>
 <p> Logo iremos entrega-la em sua residência.</p>
 
 @component('mail::button',['url' => config('app.url'), 'color' => 'green'])
