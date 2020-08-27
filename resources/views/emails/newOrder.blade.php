6a93fc6585151066ed08f92c9c8a53b422abe39e
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-08-27 00:41:37 -0300

    - Ajuste notificação nova encomenda

--- resources/views/emails/newOrder.blade.php
+++ resources/views/emails/newOrder.blade.php
@@ -1,8 +1,7 @@
 @component('mail::message')
-{{--<p> Olá {{$order->user->name}}, </p>
-<p> sua encomenda com rastreio: {{ $order->tracking }}, acaba de ser recebida. </p>--}}
-<P>Você tem uma encomenda que acabou de ser recebida.</P>
-<p>Logo iremos entrega-la em sua residência.</p>
+<p> Olá {{$order->user->name}}, </p>
+<p> sua encomenda com rastreio: {{ $order->tracking }}, acaba de ser recebida. </p>
+<p> Logo iremos entrega-la em sua residência.</p>
 
 @component('mail::button',['url' => config('app.url'), 'color' => 'green'])
     Ver minhas encomendas
