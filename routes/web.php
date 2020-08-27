7c16886c474a84afb80c41593ab7f85f753a30a2
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-08-27 00:35:22 -0300

    - Ajuste notificação nova encomenda

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('mail', function () {
-    $order = App\Models\Order::find(6);
-
-    return (new App\Notifications\NewOrder($order))
-        ->toMail($order->user);
-});
-
 Route::get('/', 'HomeController@index')->name('index');
 
 Auth::routes([ 'register' => false]);
