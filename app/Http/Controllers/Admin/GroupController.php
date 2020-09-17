c07f35d05c80aeefb1a13421079efd292ebc4261
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-09-17 17:09:42 -0300

    - Ajuste modulo de Grupos

--- app/Http/Controllers/Admin/GroupController.php
+++ app/Http/Controllers/Admin/GroupController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\GroupRequest;
-use App\Models\Models\Group;
+use App\Models\Group;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
