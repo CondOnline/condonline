7ed57ea909df1b5a67bce15d54a5d8f70a76c503
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-09-23 12:44:21 -0300

    - Ajuste Cron Queue.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //$schedule->command('queue:work --tries=3')->everyMinute();
+        $schedule->command('queue:work --daemon')->everyMinute()->withoutOverlapping();
     }
 
     /**
