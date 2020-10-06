45efee7bb019db198232b24dc742ed89282631ae
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-10-06 08:35:13 -0300

    - Create script deploy

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+# Change to the project directory
+#cd ./app
+
+# Turn on maintenance mode
+php artisan down
+
+# Pull the latest changes from the git repository
+# git reset --hard
+# git clean -df
+git pull origin master
+
+# Install/update composer dependecies
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+# Run database migrations
+php artisan migrate --force
+
+# Clear caches
+php artisan cache:clear
+
+# Clear expired password reset tokens
+php artisan auth:clear-resets
+
+# Clear and cache routes
+php artisan route:cache
+
+# Clear and cache config
+php artisan config:cache
+
+# Clear and cache views
+php artisan view:cache
+
+# Install node modules
+# npm ci
+
+# Build assets using Laravel Mix
+# npm run production
+
+#restart queues
+php artisan queue:restart
+
+# Turn off maintenance mode
+php artisan up
