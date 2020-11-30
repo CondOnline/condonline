e3df4d86b2b2f4206664cd34a95750104f407810
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-11-30 18:58:51 -0300

    - Ajuste Deploy automatizado

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 });*/
 
-Route::post('deploy', [\App\Http\Controllers\DeployController::class, 'deploy']);
+Route::get('deploy', [\App\Http\Controllers\DeployController::class, 'deploy']);
