d8be15b9b8005a5697b798311a0b72add83702c5
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-11-30 19:53:52 -0300

    - Ajuste Deploy automatizado

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -17,7 +17,10 @@ class DeployController extends Controller
 
         if (hash_equals($githubHash, $localHash)) {
             $root_path = base_path();
-            echo shell_exec('cd ' . $root_path . ' && sh ./deploy.sh');
+            $process = new Process('cd ' . $root_path . '; sh ./deploy.sh');
+            $process->run(function ($type, $buffer) {
+                echo $buffer;
+            });
         }
     }
 }
