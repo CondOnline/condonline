ca5dd8a1d2c15427b607b03ef09e1b9ba5fc264b
Diogo Ferreira <diogo@diogofmedeiros.com>
2020-12-21 02:00:05 -0300

    Ajustes Circular

--- resources/views/admin/circulars/_partials/form.blade.php
+++ resources/views/admin/circulars/_partials/form.blade.php
@@ -12,7 +12,7 @@
 <div class="form-group">
     <label>Texto</label>
     <textarea class="textarea @error('text') is-invalid @enderror" placeholder="Texto" name="text"
-              style="width: 100%; height: 400px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;" required>{{ $circular->text_mod??old('text') }}</textarea>
+              style="width: 100%; height: 400px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">{{ $circular->text_mod??old('text') }}</textarea>
 
     @error('text')
     <div class="invalid-feedback">
