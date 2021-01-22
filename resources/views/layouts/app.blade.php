39a073a2b42b07743165e2e491b5d2ae7c5b162d
Diogo Ferreira <diogo@diogofmedeiros.com>
2021-01-22 10:28:04 -0300

    Ajustes Visuais

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -143,7 +143,7 @@
 
             <!-- Sidebar Menu -->
             <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column nav-legacy" data-widget="treeview" role="menu" data-accordion="true">
+                <ul class="nav nav-pills nav-sidebar flex-column nav-legacy nav-collapse-hide-child" data-widget="treeview" role="menu" data-accordion="true">
                     <!-- Add icons to the links using the .nav-icon class
                          with font-awesome or any other icon font library -->
                     @if (auth()->user()->dweller)
@@ -372,17 +372,17 @@
             $("button").attr('disabled', 'disabled');
             $("button").text("Aguarde...");
         });
+    });
 
-        collapse_verify();
+    $(".content-wrapper").bind('click mouseover', '#link', function() {
+        if ($("body").hasClass("sidebar-collapse") && !$("aside").hasClass("sidebar-focused")) {
+            $("#condominium_name").addClass('d-none');
+        }
     });
 
     $(".main-sidebar").mouseenter(function(){
         if ($("body").hasClass("sidebar-collapse")) {
             $("#condominium_name").removeClass('d-none');
-        }
-    }).mouseleave(function() {
-        if ($("body").hasClass("sidebar-collapse")) {
-            $("#condominium_name").addClass('d-none');
         }
     });
 
