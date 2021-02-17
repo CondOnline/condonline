df2b03601493520978f2ee8f382c268d3f599d01
Diogo Ferreira <diogo@diogofmedeiros.com>
2021-02-16 22:00:30 -0300

    Ajuste na ordem cache deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,11 +21,11 @@ php artisan cache:clear
 # Clear expired password reset tokens
 php artisan auth:clear-resets
 
-# Clear and cache routes
-php artisan route:cache
-
 # Clear and cache config
 php artisan config:cache
+
+# Clear and cache routes
+php artisan route:cache
 
 # Clear and cache views
 php artisan view:cache
