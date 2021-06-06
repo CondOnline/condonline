ef975dbdd05080320fecef8f93219da0267c5a44
Diogo Ferreira <diogo@diogofmedeiros.com>
2021-06-06 12:44:34 -0300

    Incluido mascara CPF na pgina de edição do usuário

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -42,6 +42,9 @@
             //Initialize Select2 Elements
             $('.select2').select2()
         })
+
+        //Money Euro
+        $('[data-mask]').inputmask()
     </script>
 
 @endsection
