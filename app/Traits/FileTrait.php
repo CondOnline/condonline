4a07220e3ce91bfb71c4c2d14989e0583503e638
Diogo Ferreira <diogo@diogofmedeiros.com>
2021-08-15 15:37:24 -0300

    Ajuste Upload Image

--- app/Traits/FileTrait.php
+++ app/Traits/FileTrait.php
@@ -30,7 +30,7 @@ trait FileTrait
         if(is_array($files)){
             foreach ($files as $file){
                 $img = Storage::get($file);
-                $img = Image::make($img)->setFileInfoFromPath($file);
+                $img = Image::make($img);
                 $img->orientate();
                 $img->resize($w, $h, function ($constraint) {
                     $constraint->aspectRatio();
@@ -40,7 +40,8 @@ trait FileTrait
             }
         }else{
             $img = Storage::get($files);
-            $img = Image::make($img)->setFileInfoFromPath($files);
+            $img = Image::make($img);
+            dd($img->exif());
             $img->orientate();
             $img->resize($w, $h, function ($constraint) {
                 $constraint->aspectRatio();
@@ -77,6 +78,34 @@ trait FileTrait
         Storage::put($path, $img);
 
         return $path;
+    }
+
+    private function image_orientate($source, $quality = 90, $destination = null)
+    {
+        if ($destination === null) {
+            $destination = $source;
+        }
+        $info = getimagesize($source);
+        if ($info['mime'] === 'image/jpeg') {
+            $exif = exif_read_data($source);
+            if (!empty($exif['Orientation']) && in_array($exif['Orientation'], [2, 3, 4, 5, 6, 7, 8])) {
+                $image = imagecreatefromjpeg($source);
+                if (in_array($exif['Orientation'], [3, 4])) {
+                    $image = imagerotate($image, 180, 0);
+                }
+                if (in_array($exif['Orientation'], [5, 6])) {
+                    $image = imagerotate($image, -90, 0);
+                }
+                if (in_array($exif['Orientation'], [7, 8])) {
+                    $image = imagerotate($image, 90, 0);
+                }
+                if (in_array($exif['Orientation'], [2, 5, 7, 4])) {
+                    imageflip($image, IMG_FLIP_HORIZONTAL);
+                }
+                imagejpeg($image, $destination, $quality);
+            }
+        }
+        return true;
     }
 
 }
